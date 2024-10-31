bd0b20e2c34e84d99b744257450ed607223f10cf
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-10-30 21:42:10 -0300

    config cors

--- src/main/java/com/leonardolazilha/travel_api/controller/ViagemController.java
+++ src/main/java/com/leonardolazilha/travel_api/controller/ViagemController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/viagens")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class ViagemController {
 
     private final ViagemService viagemService;
